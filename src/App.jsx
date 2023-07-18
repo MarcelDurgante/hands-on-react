bc6d5389cfbce76abee130816c3c6ea5ea2418dc
MarcelDurgante <marceldur@gmail.com>
2023-07-19 01:29:11 +0200

    Added image to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className='container'>
+      <hgroup>
+        <img src='/images/group.svg' alt='StarGazers Group' />
+        <h1>Meet the StarGazers</h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      </hgroup>
+    </div>
   )
 }
 export default App
