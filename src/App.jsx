1eaf688ba742cbc5060a8d2e7bd597106a9845ff
MarcelDurgante <marceldur@gmail.com>
2023-07-19 05:25:02 +0200

    Added display selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import ListCast from './components/ListCast';
 import Support from './components/Support';
 import Welcome from "./components/Welcome"
 
 function App() {
 
+  const [memberInfo, setMemberInfo] = useState([]);
   const name = 'StarGazers';
 
   return (
@@ -14,6 +16,17 @@ function App() {
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         <ListCast />
         <Support />
+        {memberInfo &&
+          <article>
+            <hgroup>
+              <div>
+                <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+                <h1>{memberInfo.name}</h1>
+                <p>{memberInfo.bio}</p>
+              </div>
+            </hgroup>
+          </article>
+        }
       </hgroup>
     </main>
   )
