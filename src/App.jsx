9eda9049e0892cadc383a891b5a1d0f3a3d940d0
MarcelDurgante <marceldur@gmail.com>
2023-07-19 07:57:31 +0200

    Implemented Navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import ListCast from './components/ListCast';
 import Modal from './components/Modal';
+import Nav from './components/Nav';
 
 function App() {
   const [cast, setCast] = useState([]);
@@ -13,23 +14,26 @@ function App() {
 
   useEffect(() => {
     fetchCast();
-  }, [])
+  })
 
   return (
-    <main className='container'>
-      <hgroup>
-        <img src='/images/group.svg' alt='StarGazers Group' />
-        <h1>Meet the StarGazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-        {/* if using only 'memberInfo' it returns undefined first time and render an empty div. With 'memberInfo.id' it works.
+    <>
+      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+      <main className='container'>
+        <hgroup>
+          <img src='/images/group.svg' alt='StarGazers Group' />
+          <h1>Meet the StarGazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+          {/* if using only 'memberInfo' it returns undefined first time and render an empty div. With 'memberInfo.id' it works.
         Also, in the Modal component below I could not set member info to null nor undefined as it cause an error when closing the modal ( Uncaught TypeError: Cannot read properties of null/undefined (reading 'id'))
          */}
-        {(memberInfo.id &&
-          <Modal member={memberInfo} handleClose={() => { setMemberInfo([]) }} />
-        )}
-      </hgroup>
-    </main>
+          {(memberInfo.id &&
+            <Modal member={memberInfo} handleClose={() => { setMemberInfo([]) }} />
+          )}
+        </hgroup>
+      </main>
+    </>
   )
 }
 export default App
