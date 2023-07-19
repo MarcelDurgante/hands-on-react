95e35c6e8cc31d698950e3f5fc02d5b8eb9b37cf
MarcelDurgante <marceldur@gmail.com>
2023-07-19 03:14:22 +0200

    Changed files places

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,7 @@
 const Welcome = (props) => {
-    console.log(props);
+    console.log('Hi');
     return (
+        // eslint-disable-next-line react/prop-types
         <h1>Meet the <i>{props.name}</i></h1>
     );
 };
