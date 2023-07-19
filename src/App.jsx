9eec32916b68005ff90cf1b260f7de94ae5ba53d
MarcelDurgante <marceldur@gmail.com>
2023-07-19 06:31:27 +0200

    Fixed style

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,16 @@ function App() {
         {(memberInfo.id ?
           <article>
             <hgroup>
-              <div>
-                <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <h1>{memberInfo.name}</h1>
-                <p>{memberInfo.bio}</p>
+              <div style={{ display: 'flex', gap: '1rem' }}>
+                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+                <hgroup>
+                  <h1>{memberInfo.name}</h1>
+                  <p>{memberInfo.bio}</p>
+                </hgroup>
               </div>
             </hgroup>
           </article>
-          : (<div></div>))}
+          : (''))}
       </hgroup>
     </main>
   )
